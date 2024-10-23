90255572717396f560498a97b00e21d3968828bf
Hannibal B. Moulvad <hbm@impact.dk>
2024-10-23 23:17:02 +0200

    Updated StyleSheet component

--- app/_components/StyleSheet.tsx
+++ app/_components/StyleSheet.tsx
@@ -1,56 +1,10 @@
-import { Style } from "hono/css";
+import { css, Style } from "hono/css";
 
 export default function StyleSheets() {
   return (
     <>
       {/* Global styles */}
-      <style>
-        {`
-        *,
-        *::before,
-        *::after {
-          box-sizing: border-box;
-        }
-
-        * {
-          padding: 0;
-          margin: 0;
-          font-family: var(--font-system-ui);
-        }
-
-        html {
-          color-scheme: dark light;
-        }
-
-        img,
-        picture,
-        svg,
-        video {
-          display: block;
-          max-width: 100%;
-        }
-
-        h1,
-        h2,
-        h3,
-        h4,
-        h5,
-        h6 {
-          text-wrap: balance;
-        }
-
-        p {
-          text-wrap: pretty;
-        }
-
-        @media (prefers-reduced-motion: no-preference) {
-          :has(:target) {
-            scroll-behavior: smooth;
-            scroll-padding-top: var(--size-9);
-          }
-        }
-      `}
-      </style>
+      <Style>{resetStyles}</Style>
       {/* Open Props Variables */}
       <link
         rel="stylesheet"
@@ -68,8 +22,52 @@ export default function StyleSheets() {
         rel="stylesheet"
         href="https://unpkg.com/open-props@1.7.5/borders.min.css"
       />
-      {/* Hono CSS */}
-      <Style />
     </>
   );
 }
+
+const resetStyles = css`
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: var(--font-system-ui);
+  }
+
+  html {
+    color-scheme: dark light;
+  }
+
+  img,
+  picture,
+  svg,
+  video {
+    display: block;
+    max-width: 100%;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    text-wrap: balance;
+  }
+
+  p {
+    text-wrap: pretty;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    :has(:target) {
+      scroll-behavior: smooth;
+      scroll-padding-top: var(--size-9);
+    }
+  }
+`;
